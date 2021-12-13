033c03b7127b279beedc6e11a36811d590bd47e6
hassan3111 <ali.hassan@student.moringaschool.com>
2021-12-13 22:06:37 +0300

    add time zone

--- awwards/settings.py
+++ awwards/settings.py
@@ -165,7 +165,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
