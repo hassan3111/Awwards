cf117ca82f540160c0f4d46f65247f95a93bdebe
hassan3111 <ali.hassan@student.moringaschool.com>
2021-12-13 20:53:33 +0300

    add static files storage in setting.py

--- awwards/settings.py
+++ awwards/settings.py
@@ -137,6 +137,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 
